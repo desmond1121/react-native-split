01f3b199760827f18aa6e2b5818ea7755fe469c7
shenyunlong <shenyunlong@qiyi.com>
2017-04-25 13:40:27 +0800

    Fix run example failed

--- split-example/run-example.sh
+++ split-example/run-example.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+mkdir build
 node ../index.js --platform android --output build --config .splitconfig --dev true
 
-mkdirs android/app/src/main/assets/bundle
+mkdir android/app/src/main/assets/bundle
 rm -rf android/app/src/main/assets/bundle/*
 cp -R build/bundle-output/split/* android/app/src/main/assets/bundle
 cd android
