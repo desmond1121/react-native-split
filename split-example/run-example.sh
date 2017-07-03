982fc0edd6daed41cd1687023235394438f80712
zhexi.yjy <zhexi.yjy@antfin.com>
2017-07-03 10:29:26 +0800

    fix #5, asset dir not createls

--- split-example/run-example.sh
+++ split-example/run-example.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-mkdir build
+if [ ! -d 'build' ]; then 
+  mkdir build
+fi
+
+if [ ! -d 'android/app/src/main/assets/bundle' ]; then 
+  mkdir -p android/app/src/main/assets/bundle
+fi
+
 node ../index.js --platform android --output build --config .splitconfig --dev false
 
 rm -rf android/app/src/main/assets/bundle/*
